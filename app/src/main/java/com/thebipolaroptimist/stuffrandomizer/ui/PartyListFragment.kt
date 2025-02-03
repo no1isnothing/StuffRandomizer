c8b96589196494c012b4613d1f32ee207ee66990
no1isnothing <no1isnothing@gmail.com>
2025-02-03 10:03:06 -0600

    Compose Migration: PartyListFragment #19

--- app/src/main/java/com/thebipolaroptimist/stuffrandomizer/ui/PartyListFragment.kt
+++ app/src/main/java/com/thebipolaroptimist/stuffrandomizer/ui/PartyListFragment.kt
@@ -5,9 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.stringResource
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.thebipolaroptimist.stuffrandomizer.R
@@ -17,87 +39,68 @@ import com.google.common.flogger.FluentLogger
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
- * A [RecyclerView.Adapter] for displaying [Party]s.
- */
-class PartyListAdapter(private val partyList: ArrayList<Party>) :
-    RecyclerView.Adapter<PartyListAdapter.ViewHolder>() {
-
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val partyName: TextView = view.findViewById(R.id.partyName)
-        val assigneePreview: TextView = view.findViewById(R.id.partyAssigneesPreview)
-    }
-
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.item_party, viewGroup, false)
-
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        logger.atInfo().log("Binding view")
-        viewHolder.partyName.text = partyList[position].partyName
-        viewHolder.assigneePreview.text =
-            partyList[position].members.joinToString { member -> member.assignee }
-        viewHolder.partyName.setOnClickListener { v ->
-            val bundle = Bundle()
-            bundle.putString(v.resources.getString(R.string.key_uuid),
-                partyList[position].uid.toString())
-            v.findNavController()
-                .navigate(R.id.action_PartyListFragment_to_PartyEditFragment,
-                    bundle)
-        }
-    }
-
-    override fun getItemCount() = partyList.size
-
-    companion object {
-        private val logger: FluentLogger = FluentLogger.forEnclosingClass()
-    }
-}
-
-/**
  * A [Fragment] for displaying [Party]s.
  */
 @AndroidEntryPoint
 class PartyListFragment : Fragment() {
     private val mainViewModel: MainViewModel by activityViewModels()
 
-    private var _binding: FragmentPartyListBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-    private val partyList = arrayListOf<Party>()
-    private val partyAdapter = PartyListAdapter(partyList)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentPartyListBinding.inflate(inflater, container, false)
-
-        val recyclerView: RecyclerView = binding.partyList
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        recyclerView.adapter = partyAdapter
-
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        mainViewModel.parties.observe(viewLifecycleOwner) { parties ->
-            partyList.clear()
-            logger.atInfo().log("Setting Parties size %d", parties.size)
-            partyList.addAll(parties)
-            partyAdapter.notifyDataSetChanged()
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent { PartyListScreen() }
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    @Composable
+    fun PartyListScreen() {
+        val partyList by mainViewModel.parties.observeAsState(listOf())
+
+        Scaffold(
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = { findNavController().navigate(R.id.action_PartyListFragment_to_PartyCreationFragment) },
+                ) {
+                    Icon(Icons.Filled.Add, stringResource(R.string.add_match))
+                }
+            }) {
+                padding ->
+            LazyColumn(Modifier.fillMaxSize()
+                .padding(padding)) {
+                items(partyList) { item ->
+                    PartyItem(item)
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun PartyItem(party: Party) {
+        logger.atInfo().log("Creating Party %s", party.partyName)
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .clickable {
+                    val bundle = Bundle()
+                    bundle.putString(
+                        resources.getString(R.string.key_uuid),
+                        party.uid.toString()
+                    )
+                    findNavController()
+                        .navigate(
+                            R.id.action_PartyListFragment_to_PartyEditFragment,
+                            bundle
+                        )
+                }) {
+            Column {
+                Text(party.partyName)
+                Text(party.members.joinToString { member -> member.assignee })
+            }
+        }
     }
 
     companion object {
