7bbd446bc672e31ad469de29890a6017619bb10f
no1isnothing <no1isnothing@gmail.com>
2025-03-17 12:22:38 -0500

    UI/UX Improvements: PartyEditScreen #20

--- app/src/main/java/com/thebipolaroptimist/stuffrandomizer/ui/PartyEditScreen.kt
+++ app/src/main/java/com/thebipolaroptimist/stuffrandomizer/ui/PartyEditScreen.kt
@@ -6,23 +6,25 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.thebipolaroptimist.stuffrandomizer.R
 import com.thebipolaroptimist.stuffrandomizer.MainViewModel
-import com.thebipolaroptimist.stuffrandomizer.data.Member
+import com.thebipolaroptimist.stuffrandomizer.R
+import com.thebipolaroptimist.stuffrandomizer.ui.components.ClickableTwoLineItem
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -49,11 +51,39 @@ fun PartyEditScreen(
                             contentDescription = stringResource(id = R.string.back)
                         )
                     }
+                },
+                actions = {
+                    IconButton(onClick = {
+                        if (party != null) {
+                            mainViewModel.rerollEditPartyMembers(party)
+                        }
+                    }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.casino_24px),
+                            contentDescription = stringResource(
+                                id = R.string.roll
+                            )
+                        )
+                    }
+                    IconButton(onClick = {
+                        if (party != null) {
+                            mainViewModel.saveEditedParty(party)
+                        }
+
+                        navigateBack()
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Done,
+                            contentDescription = stringResource(R.string.save)
+                        )
+                    }
                 })
         },
     ) { padding ->
         Column(Modifier.padding(padding)) {
-            TextField(value = mainViewModel.editPartyName,
+            OutlinedTextField(
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_small)),
+                value = mainViewModel.editPartyName,
                 onValueChange = { mainViewModel.editPartyName = it },
                 label = { Text(stringResource(R.string.hint_match_name)) }
             )
@@ -62,37 +92,14 @@ fun PartyEditScreen(
                     .weight(1f)
             ) {
                 items(mainViewModel.editPartyMembers) { item ->
-                    MemberEditItem(member = item)
+                    ClickableTwoLineItem(item.assignee,
+                        item.assignments
+                            .map { assignment -> assignment.key + ": " + assignment.value }
+                            .joinToString()
+                    )
                 }
-            }
-            Button(onClick = {
-                if (party != null) {
-                    mainViewModel.rerollEditPartyMembers(party)
-                }
-            }) {
-                Text(text = stringResource(R.string.reroll))
-            }
-            Button(onClick = {
-                if (party != null) {
-                    mainViewModel.saveEditedParty(party)
-                }
-
-                navigateBack()
-            }) {
-                Text(text = stringResource(R.string.save))
             }
         }
     }
 
-}
-
-@Composable
-fun MemberEditItem(member: Member) {
-    Column {
-        Text(member.assignee)
-        Text(
-            member.assignments
-                .map { assignment -> assignment.key + ": " + assignment.value }.joinToString()
-        )
-    }
 }
